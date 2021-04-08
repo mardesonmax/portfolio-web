1fefb3a4697e7a0cc8b6fcdbbe0a27c4e10ce2ae
Mardeson Pereira <maxpb777@gmail.com>
2021-04-08 17:26:14 -0300

    Project Create

---
+++ src/pages/ProjectCreate/styled.ts
@@ -0,0 +1,40 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+`;
+const animeTop = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(50px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const Content = styled.div`
+  margin: 15px;
+  width: 100%;
+  max-width: 700px;
+  border-radius: 5px;
+
+  form {
+    animation: ${animeTop} 0.5s;
+    margin-top: 15px;
+
+    .submit-button {
+      display: flex;
+      justify-content: flex-end;
+
+      button {
+        width: auto;
+        margin: 0;
+        margin: 15px 0;
+      }
+    }
+  }
+`;
