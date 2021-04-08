6e9a1ad3672a30d0eb06dfd1020601f4ba58620f
Mardeson Pereira <maxpb777@gmail.com>
2021-04-08 17:29:51 -0300

    Estilizando About

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,71 +1,155 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import { FiChevronLeft, FiPlus } from 'react-icons/fi';
+import * as Yup from 'yup';
 
+import { toast } from 'react-toastify';
 import PageHeader from '../../components/PageHeader';
 
 import { Container, Content, AboutContact } from './styled';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { useAuth } from '../../hooks/auth';
 import Textarea from '../../components/Textarea';
+import ButtonLink from '../../components/ButtonLink';
+import api from '../../services/api';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+interface IAbout {
+  id: string;
+  title: string;
+  description: string;
+}
 
 const About: React.FC = () => {
-  const { user } = useAuth();
   const formRef = useRef<FormHandles>(null);
+  const [abouts, setAbouts] = useState<IAbout[]>([]);
+  const [aboutEdit, setAboutEdit] = useState('');
+  const [aboutPlus, setAboutPlus] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('/abouts');
+      setAbouts(response.data);
+    })();
+  }, []);
+
+  const handleSubmit = useCallback(async (data) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        title: Yup.string().required('Titulo obrigatório'),
+        description: Yup.string().required('Descrição obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      const response = await api.post('/abouts', data);
+
+      setAbouts((state) => [...state, response.data]);
+      formRef.current?.reset();
+
+      toast('Informações salvas com sucesso.', {
+        type: 'success',
+      });
+
+      setAboutPlus(false);
+
+      window.scrollTo(0, document.querySelector('html')?.scrollHeight || 0);
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        return;
+      }
+
+      toast('Algo deu errado, tente novamente.', {
+        type: 'error',
+      });
+    }
+  }, []);
+
+  const handleAbort = useCallback(() => {
+    setAboutEdit('');
+  }, []);
+
+  const handleAboutEdit = useCallback((id: string) => {
+    setAboutPlus(false);
+    setAboutEdit(id);
+  }, []);
+
+  const handleAbortPlus = useCallback(() => {
+    setAboutPlus(true);
+    setAboutEdit('');
+  }, []);
+
   return (
     <Container>
       <Content>
-        <PageHeader title="Sobre mim" />
+        <PageHeader title="Sobre mim">
+          <div>
+            <Button onClick={handleAbortPlus}>
+              <FiPlus />
+              <span>Adicionar</span>
+            </Button>
+            <ButtonLink to="/profile">
+              <FiChevronLeft />
+              <span>Voltar</span>
+            </ButtonLink>
+          </div>
+        </PageHeader>
         <div className="about">
-          <Form ref={formRef} onSubmit={() => console.log('ok')}>
-            <Input label="Título" name="title" />
-            <Textarea label="Descrição" name="description" />
-            <div>
-              <Button>Concluir</Button>
-            </div>
-          </Form>
+          {aboutPlus && (
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input label="Título" name="title" />
+              <Textarea label="Descrição" name="description" />
+              <div>
+                <Button type="submit">Enviar</Button>
+              </div>
+            </Form>
+          )}
 
           <AboutContact>
-            <div className="item">
-              <h2>Titulo sobre mim</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </p>
-            </div>
-
-            <div className="item">
-              <h2>Titulo sobre mim</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </p>
-            </div>
-
-            <div className="item">
-              <h2>Titulo sobre mim</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </p>
-            </div>
-
-            <div className="item">
-              <h2>Titulo sobre mim</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </p>
-            </div>
+            {abouts &&
+              abouts.map((about) =>
+                about.id === aboutEdit ? (
+                  <Form key={about.id} ref={formRef} onSubmit={handleSubmit}>
+                    <Input
+                      label="Título"
+                      name="title"
+                      defaultValue={about.title}
+                    />
+                    <Textarea
+                      label="Descrição"
+                      name="description"
+                      defaultValue={about.description}
+                    />
+                    <div>
+                      <Button
+                        bgColor="primary"
+                        type="button"
+                        onClick={handleAbort}
+                      >
+                        Cancelar
+                      </Button>
+                      <Button type="submit">Concluir</Button>
+                    </div>
+                  </Form>
+                ) : (
+                  <div key={about.id} className="item">
+                    <h2>{about.title}</h2>
+                    <p>{about.description}</p>
+                    <div>
+                      <Button onClick={() => handleAboutEdit(about.id)}>
+                        Editar
+                      </Button>
+                      <Button bgColor="secondary">Excluir</Button>
+                    </div>
+                  </div>
+                ),
+              )}
           </AboutContact>
         </div>
       </Content>
--- src/pages/About/styled.ts
+++ src/pages/About/styled.ts
@@ -1,26 +1,48 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
   justify-content: center;
 `;
 
+const animeLeft = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const animeRight = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
 export const Content = styled.div`
   margin: 15px;
-  max-width: 1200px;
   width: 100%;
+  max-width: 700px;
   border-radius: 5px;
 
   .about {
     display: flex;
-    flex-wrap: wrap;
-    padding: 15px 0px;
+    flex-direction: column;
+    padding: 15px 0;
   }
 
   form {
-    flex: 1;
-    margin-right: 7.5px;
-    min-width: 350px;
+    animation: ${animeLeft} 0.5s;
 
     > div {
       display: flex;
@@ -31,32 +53,67 @@ export const Content = styled.div`
       }
     }
   }
-
-  @media (max-width: 768px) {
-    .about {
-      form {
-        margin-right: 0;
-      }
-
-      .item {
-        margin-left: 0;
-      }
-    }
-  }
 `;
 
 export const AboutContact = styled.div`
-  flex: 1;
-  min-width: 350px;
+  form {
+    border: 2px solid ${(props) => props.theme.primary};
+    border-radius: 5px;
+    margin-bottom: 15px;
+
+    label {
+      margin: 15px;
+      width: auto;
+    }
+
+    padding-right: 0;
+    > div {
+      justify-content: space-between;
+      padding: 0 15px;
+    }
+  }
 
   .item {
+    animation: ${animeRight} 0.5s;
     background: ${(props) => props.theme.container};
     box-shadow: ${(props) => props.theme.boxShadow};
     margin-bottom: 15px;
     padding: 15px;
     border-radius: 5px;
-    margin-left: 7.5px;
-
     color: ${(props) => props.theme.textDark};
+    position: relative;
+    overflow: hidden;
+
+    > div {
+      display: flex;
+      width: 100%;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      padding: 15px;
+      position: absolute;
+      justify-content: space-around;
+      align-items: center;
+      opacity: 0;
+      visibility: hidden;
+      transition: 0.3s ease all;
+      backdrop-filter: blur(3px);
+
+      > div {
+        margin: 0 5px;
+      }
+    }
+
+    &:hover {
+      > div {
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding-left: 0;
+    flex: 100%;
   }
 `;
