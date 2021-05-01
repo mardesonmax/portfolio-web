3310be87dc4de010826a2ff7328d4c863bdbf5bb
Mardeson Pereira <maxpb777@gmail.com>
2021-05-01 12:59:20 -0300

    home background color

--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -57,7 +57,7 @@ export const Container = styled.div`
   width: 100vw;
   padding: 20px;
   top: 0;
-  background: ${(props) => props.theme.background};
+  background: ${(props) => props.theme.container};
   position: absolute;
   z-index: 9999;
   overflow: hidden;
